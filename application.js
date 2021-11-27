e457413519189024009b07a2a29aad72050b9afe
Sarbjot Mann <sarbjotm@sfu.ca>
2021-11-26 18:00:15 -0800

    Extra q value broke everything

--- application.js
+++ application.js
@@ -1,31 +1,31 @@
 
-let counter = 0
-let miscounter = 0
-let level = 1
-let colours = ['#f1d376', '#df87df', '#22c66a', '#22a8c6', '#db6bf7']
+let counter = 0;
+let miscounter = 0;
+let level = 1;
+let colours = ['#f1d376', '#df87df', '#22c66a', '#22a8c6', '#db6bf7'];
+
 addsquares=function(){
-  i = 0
-  var v = $('#howmany').val()
+  i = 0;
+  var v = $('#howmany').val();
   if (v == ""){
-    alert("Please enter a value greater than 0")
+    alert("Please enter a value greater than 0");
   }
 
   else if (v == 0){
-    alert ("To play the game, you must enter a number greater than 0")
+    alert ("To play the game, you must enter a number greater than 0");
   }
 
-  else{
+  else if (v <= 50){
 
-
-    for(i= 0; i<v; i+=1){
+    for(i = 0; i < v; i+=1){
       random_colour = Math.floor(Math.random() * (4 - 0 + 1) + 0);
       min = Math.ceil(1);
       max = Math.floor(2);
       shape_style = Math.floor(Math.random() * (max - min + 1) + min);
 
       if(shape_style == 1){
-        x = Math.random() * 360;
-        y = Math.random() * 360;
+        x = Math.floor(Math.random() * (460 - 30 + 1) + 30);
+        y = Math.floor(Math.random() * (460 - 30 + 1) + 30);
         r = paper.rect(x, y, 20, 20);
         filled = {'fill': colours[random_colour]};
 
@@ -37,20 +37,31 @@ addsquares=function(){
       }
 
       else{
-        x = Math.random() * 360;
-        y = Math.random() * 360;
-        r = paper.ellipse(x, y, 40, 20 )
+        x = Math.floor(Math.random() * (460 - 30 + 1) + 30);
+        y = Math.floor(Math.random() * (460 - 30 + 1) + 30);
+        r = paper.ellipse(x, y, 40, 20)
         filled = {'fill': colours[random_colour]};
-        r.attr(filled)
+        r.attr(filled);
         rotate = Math.random()*1440-720;
         turning = {'transform': 'r'+rotate};
         r.animate(turning,4000, misses);
         r.click(removal);
 
-
       }
-    }
+
   }
+
+}
+
+  else if (v > 50){
+    alert("Please enter a value less than or equal to 50")
+  }
+
+
+  else{
+    alert("Please enter an integer value")
+  }
+
 }
 
 
@@ -79,6 +90,7 @@ addcircles = function(){
   circle.attr("stroke", colours[random_colour]);
   c = c + 10;
 }
+
 
 setup=function(){
 
